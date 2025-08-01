025b1a80b826d620a3aa75643f5b8770f238dc86
Egmarortiz <egmarocasio9@gmail.com>
2025-07-31 22:37:01 -0400

    sum of list items

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module sums up mixed list"""
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Sum up a mixed list"""
+    return sum(mxd_lst)
