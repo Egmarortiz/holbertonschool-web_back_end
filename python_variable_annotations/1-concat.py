e5602232b2f067c750043e998a63499c4b17ea53
Egmarortiz <egmarocasio9@gmail.com>
2025-07-31 22:05:04 -0400

    concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Module works with string concatenation
+"""
+def concat(str1: str, str2: str) -> str:
+    """Function concatenates two strings"""
+    return str1 + str2
