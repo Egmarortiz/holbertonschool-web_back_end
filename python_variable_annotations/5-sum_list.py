ba035f9fea243b5af913f6c42edd15073e73bacf
Egmarortiz <egmarocasio9@gmail.com>
2025-07-31 22:30:25 -0400

    sum of list items

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Sum of list items"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Return sum of list items"""
+    return float(input_list[0] + input_list[1])
