d2a368e685933f1c941fe7ee7216c733e300bd40
Egmarortiz <egmarocasio9@gmail.com>
2025-08-01 19:17:51 -0400

    basic async

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Basic asynn syntax"""
+
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Function return await time"""
+    delay_time = random.uniform(0, max_delay)
+    await asyncio.sleep(delay_time)
+    return delay_time
---
+++ python_async_function/demo.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import time
+
+for i in range(5):
+    print(f"Fetching page {i}")
+    time.sleep(3)  # wait 1 second between requests
