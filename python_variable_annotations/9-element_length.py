66c98101720121770daa5958a4a212ecb48fe3bd
Egmarortiz <egmarocasio9@gmail.com>
2025-08-01 00:08:25 -0400

    duck typing

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module returns list of tuples"""
+
+
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """Function returns list of tuples"""
+    return [(i, len(i)) for i in lst]
