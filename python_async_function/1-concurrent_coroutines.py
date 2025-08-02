aa39aac7e4caad7412929c92e96dcd339b9a2b52
Egmarortiz <egmarocasio9@gmail.com>
2025-08-01 20:40:33 -0400

    multiple delay times

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module executes mutiple corotuines"""
+
+
+
+from typing import List
+import asyncio
+from importlib import import_module
+
+wait_random = import_module('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Function returns lists of delay times"""
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+
+    delays = []
+    for coro in asyncio.as_completed(coroutines):
+        delay = await coro
+        delays.append(delay)
+
+    return delays
