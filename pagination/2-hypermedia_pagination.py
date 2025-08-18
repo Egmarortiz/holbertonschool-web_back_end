4b05d53d7319322604f70d5157cb105a18d05ae5
Egmarortiz <egmarocasio9@gmail.com>
2025-08-18 17:43:51 -0400

    hypermedia pagination

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""Module Paginates a CSV file"""
+
+
+import csv
+import math
+from typing import List, Tuple, Dict, Any
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Module returns offset and limit"""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return start, end
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Method paginates csv file"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = self.dataset()
+        start, end = index_range(page, page_size)
+
+        return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """
+        Method returns pagination meta data
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstsnce(page, int) and page_size > 0
+
+        data_page = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size) if total_items else 0
+
+        return {
+            "page_size": len(data_page)
+            "page": page,
+            "data": data_page,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > total_pages else None,
+            "total_pages": total_pages,
+        }
