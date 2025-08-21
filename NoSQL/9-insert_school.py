c41f3b330d30e91a48f05112f0f3c7a60bc55a2c
Egmarortiz <egmarocasio9@gmail.com>
2025-08-21 19:07:04 -0400

    insert

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""Insert document in collection"""
+
 
 def insert_school(mongo_collection, **kwargs):
+    """Module prints id of new doc"""
     result =  mongo_collection.insert_one(kwargs)
     return result.inserted_id
