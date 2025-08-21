3b0ee668b2560a9e04b89c78dcadb87753ab7c4e
Egmarortiz <egmarocasio9@gmail.com>
2025-08-21 19:04:30 -0400

    printing collection

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""List all documents in a collection"""
+
 
 def list_all(mongo_collection):
+    """Prints all documents in a collection"""
     docs =  list(mongo_collection.find())
     return docs
