2f37aa7274bfa66c130769773696873b75c6888b
Egmarortiz <egmarocasio9@gmail.com>
2025-08-21 19:30:27 -0400

    update

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/ python3
+"""Module updates document arguments"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Module updates document arguments"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
