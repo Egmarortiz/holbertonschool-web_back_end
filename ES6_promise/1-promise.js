384c817136e84e91ee71da319a86b39ac5f139ec
Egmarortiz <egmarocasio9@gmail.com>
2025-09-05 20:08:17 -0400

    basic promise

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,13 +1,13 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-
-    const returnValue = {status: 200, body: "Success"};
-
-    if (success === true) {
-      resolve(returnValue);
+    if (success) {
+      resolve({
+        status: 200,
+        body: "Success",
+      });
     }
     else {
-      reject("The fake API is not working currently");
+      reject(new Error("The fake API is not working currently"));
     }
   });
 }
