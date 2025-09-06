c745a38cc7dc8554853872169d296586fbc27ce5
Egmarortiz <egmarocasio9@gmail.com>
2025-09-05 20:14:59 -0400

    clean at the end

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,16 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error(''));
+    }
+  }).then((response) => {
+    console.log('Got a response from the API');
+    return response;
+});
+}
+
