77a58c0f8d1ea07e24969b21fc293084793cf882
Egmarortiz <egmarocasio9@gmail.com>
2025-09-05 20:42:37 -0400

    handling promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,16 +1,15 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      return ({
         status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(new Error(''));
-    }
-  }).then((response) => {
-    console.log('Got a response from the API');
-    return response;
-});
+        body: 'success',
+      })
+    })
+    .catch(() => {
+      return new Error()
+    })
+    .finally(() => {
+      console.log("Got a response from the API");
+    });
 }
-
