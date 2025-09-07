12946392acbcc88759bef1b2beaabc0fd2da3afb
Egmarortiz <egmarocasio9@gmail.com>
2025-09-06 20:27:47 -0400

    all settled

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,16 +5,14 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => {
-    if (result.status === 'fulfilled') {
-      return {
-        status: result.status,
-        value: result.value,
-      };
-    }
-    return {
+  ]).then((results) =>
+    results.map((result) => ({
       status: result.status,
-      value: result.reason instanceof Error ? result.reason.message : result.reason,
-    };
-  }));
+      value:
+        result.status === 'fulfilled'
+          ? result.value
+          : result.reason.message || result.reason,
+    }))
+  );
 }
+
